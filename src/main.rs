15b470a07dee715cfa567ca372e1f1ac0d012a2c
Aidan De Angelis <aidandeangelis66@gmail.com>
2026-05-08 18:56:55 -0700

    test: add dummy code and test for CI validation
    Skip doctest for binary-only crates (no lib target).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a.saturating_add(b)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 1), 2);
+        assert_eq!(add(0, 0), 0);
+        assert_eq!(add(-1, 1), 0);
+    }
 }
