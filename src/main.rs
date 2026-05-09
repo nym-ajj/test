a6f4e7b02fd261e71402c2c9a9b60f0fbf0ea136
Aidan De Angelis <aidandeangelis66@gmail.com>
2026-05-08 19:03:09 -0700

    Revert "test: add dummy code and test for CI validation"
    This reverts commit 15b470a07dee715cfa567ca372e1f1ac0d012a2c.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
 fn main() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-}
-
-fn add(a: i32, b: i32) -> i32 {
-    a.saturating_add(b)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 1), 2);
-        assert_eq!(add(0, 0), 0);
-        assert_eq!(add(-1, 1), 0);
-    }
+    println!("Hello, world!");
 }
